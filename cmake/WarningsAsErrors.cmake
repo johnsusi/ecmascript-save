9b234670591fdf2b73c4e4ff269a078e11804f55
John Susi <john@susi.se>
2017-05-30 11:45:16 +0200

    Cleanup

--- cmake/WarningsAsErrors.cmake
+++ cmake/WarningsAsErrors.cmake
@@ -1,8 +1,9 @@
 
 include(CheckCXXCompilerFlag)
 
-CHECK_CXX_COMPILER_FLAG(-Wall   WALL_SUPPORTED)
-CHECK_CXX_COMPILER_FLAG(-Werror WERROR_SUPPORTED)
+CHECK_CXX_COMPILER_FLAG(-Wall      WALL_SUPPORTED)
+CHECK_CXX_COMPILER_FLAG(-Werror    WERROR_SUPPORTED)
+CHECK_CXX_COMPILER_FLAG(-Wpedantic WPEDANTIC_SUPPORTED)
 
 if (WALL_SUPPORTED)
   add_compile_options(-Wall)
@@ -10,4 +11,8 @@ endif()
 
 if (WERROR_SUPPORTED)
   add_compile_options(-Werror)
+endif()
+
+if (WPEDANTIC_SUPPORTED)
+  add_compile_options(-Wpedantic)
 endif()
